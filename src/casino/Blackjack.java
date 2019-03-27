1c88e9082eb7b8690beb011e6372974782cba659
Steven Kozlowski <skozlowski@ucvts.tec.nj.us>
2019-03-26 21:59:44 -0400

    Final commit

--- src/casino/Blackjack.java
+++ src/casino/Blackjack.java
@@ -1,5 +1,7 @@
 package casino;
 
+import java.util.Scanner;
+
 public class Blackjack {
 
 	public static void main(String[] args) {
@@ -10,9 +12,102 @@ public class Blackjack {
 		playingDeck.createFullDeck();
 		playingDeck.shuffle();
 		
+		Deck playerDeck = new Deck();
 		
-		System.out.println(playingDeck);
-
+		Deck dealerDeck = new Deck();
+		
+		double playerMoney = 100.00;
+		
+		Scanner userInput = new Scanner(System.in);
+		
+		while(playerMoney > 0) {
+			System.out.println("You have $" + playerMoney + ", how much do you want to bet?");
+			double playerBet = userInput.nextDouble();
+			if(playerBet > playerMoney) {
+				System.out.println("You can't bet more money than you have.");
+				break;
+			}
+			
+			boolean endRound = false;
+			
+			playerDeck.draw(playingDeck);
+			playerDeck.draw(playingDeck);
+			
+			dealerDeck.draw(playingDeck);
+			dealerDeck.draw(playingDeck);
+			
+			while(true) {
+				System.out.println("Your hand:");
+				System.out.println(playerDeck.toString());
+				System.out.println("Your hand is valued at: " + playerDeck.cardsValue());
+				
+				System.out.println("Dealer hand: " + dealerDeck.getCard(0).toString() + " and [Hidden]");
+				
+				System.out.println("Do you want to (1)Hit or (2)Stand?");
+				int response = userInput.nextInt();
+				
+				if(response == 1) {
+					playerDeck.draw(playingDeck);
+					System.out.println("You drew a " + playerDeck.getCard(playerDeck.deckSize()-1).toString());
+					
+					if(playerDeck.cardsValue() > 21) {
+						System.out.println("Bust.  Currently valued at: " + playerDeck.cardsValue());
+						playerMoney -= playerBet;
+						endRound = true;
+					}
+				}
+				
+				if(response == 2) {
+					break;
+				}
+			}
+			
+			System.out.println("Dealer Cards: " + dealerDeck.toString());
+			
+			if(dealerDeck.cardsValue() > playerDeck.cardsValue() && endRound == false) {
+				System.out.println("Dealer wins.");
+				playerMoney -= playerBet;
+				endRound = true;
+			}
+			
+			while(dealerDeck.cardsValue() < 17 && endRound == false) {
+				dealerDeck.draw(playingDeck);
+				System.out.println("Dealer draws: " + dealerDeck.getCard(dealerDeck.deckSize()-1).toString());
+			}
+			
+			System.out.println("Dealer's hand is valued at: " + dealerDeck.cardsValue());
+			
+			if((dealerDeck.cardsValue() > 21) && endRound == false) {
+				System.out.println("Dealer busts.  You win.");
+				playerMoney += playerBet;
+				endRound = true;
+			}
+			
+			if((playerDeck.cardsValue() == dealerDeck.cardsValue()) && endRound == false) {
+				System.out.println("Push.");
+				endRound = true;
+			}
+			
+			if((playerDeck.cardsValue() > dealerDeck.cardsValue()) && endRound == false) {
+				System.out.println("You win the hand.");
+				playerMoney += playerBet;
+				endRound = true;
+			}
+			
+			else if(endRound == false) {
+				System.out.println("You lose the hand.");
+				playerMoney -= playerBet;
+				endRound = true;
+			}
+			
+			playerDeck.moveAllToDeck(playingDeck);
+			dealerDeck.moveAllToDeck(playingDeck);
+			System.out.println("End of hand.");
+			
+		}
+		
+		System.out.println("Game over.  You're out of money.");
+		
 	}
 
 }
--- src/casino/Deck.java
+++ src/casino/Deck.java
@@ -36,11 +36,81 @@ public class Deck {
 	
 	public String toString() {
 		String cardListOutput = "";
-		int i = 0;
 		for(Card aCard : this.cards) {
-			cardListOutput += "\n" + i + "-" + aCard.toString();
-			i++;
+			cardListOutput += "\n " + aCard.toString();
 		}
 		return cardListOutput;
+		
 	}
+	
+	public void removeCard(int i) {
+		this.cards.remove(i);
+	}
+	
+	public Card getCard(int i) {
+		return this.cards.get(i);
+	}
+	
+	public void addCard(Card addCard) {
+		this.cards.add(addCard);
+	}
+	
+	public void draw(Deck comingFrom) {
+		this.cards.add(comingFrom.getCard(0));
+		comingFrom.removeCard(0);
+	}
+	
+	public int deckSize() {
+		return this.cards.size();
+	}
+	
+	public void moveAllToDeck(Deck moveTo) {
+		int thisDeckSize = this.cards.size();
+		
+		for(int i = 0; i < this.deckSize(); i++) {
+			moveTo.addCard(this.getCard(i));
+		}
+		
+		for(int i = 0; i < thisDeckSize; i++) {
+			this.removeCard(0);
+		}
+		
+	}
+	
+	public int cardsValue() {
+		int totalValue = 0;
+		int aces = 0;
+		
+		for(Card aCard : this.cards) {
+			switch(aCard.getValue()) {
+			case TWO: totalValue += 2; break;
+			case THREE: totalValue += 3; break;
+			case FOUR: totalValue += 4; break;
+			case FIVE: totalValue += 5; break;
+			case SIX: totalValue += 6; break;
+			case SEVEN: totalValue += 7; break;
+			case EIGHT: totalValue += 8; break;
+			case NINE: totalValue += 9; break;
+			case TEN: totalValue += 10; break;
+			case JACK: totalValue += 10; break;
+			case QUEEN: totalValue += 10; break;
+			case KING: totalValue += 10; break;
+			case ACE: aces += 1; break;
+			}
+		}
+		
+		for(int i = 0; i < aces; i++) {
+			
+			if(totalValue > 10) {
+				totalValue += 1;
+			}
+			else {
+				totalValue += 11;
+			}
+		}
+		
+		return totalValue;
+		
+	}
+	
 }
